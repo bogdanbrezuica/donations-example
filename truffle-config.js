282259e1d73cc3b73e81389a5a238f4fddaa6039
Bogdan Brezuica <bogdan.brezuica@akasha.world>
2018-08-09 11:50:13 +0300

    deploy v1 to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -7,6 +7,13 @@ module.exports = {
       port: 9545,
       gas: 5000000,
       network_id: '*'
+    },
+    rinkeby: {
+      name: 'rinkeby',
+      host: 'localhost',
+      port: '8545',
+      gas: 5000000,
+      network_id: 4
     }
   }
 };
